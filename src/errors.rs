ead6d73be2db37492edf5933a9b6185dbd15615e
noobsiecoder <noobsiecoder@gmail.com>
2024-07-08 00:06:33 +0530

    modified errors for core, io and imgproc

--- src/errors.rs
+++ src/errors.rs
@@ -1,71 +1,45 @@
-// MIT License
+use core::fmt;
+use std::error::Error;
 
-// Copyright (c) 2024 Abhishek Sriram
-
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
-
-/// Enum to indicate the errorcode during runtime
 ///
-/// # Examples
-/// ```
-/// # use vision_x::errors;
-/// use vision_x::{errors::ErrorCode, image};
+pub type VisionXError = Box<dyn Error + 'static>;
 ///
-/// # fn main() -> Result<(), errors::Error> {
-/// let path = "images/png/xyz.png";
-/// let image_data = image::load(&path);
-///
-/// assert_eq!(image_data.unwrap_err().code, ErrorCode::PathNotFound);
-/// # Ok(()) }
-/// ```
-#[derive(Debug, Clone, PartialEq)]
-pub enum ErrorCode {
-    PathNotFound,
-    FileNotFound,
-    InvalidExtension,
-    InvalidColorExtension,
-    IOError,
-    InternalError,
+pub type VisionXResult<T> = Result<T, VisionXError>;
+
+/// enum VisionXErrorKind is a custom errorkind
+#[derive(Debug)]
+pub enum VisionXErrorKind {
+    //
+    IndexOutofBound(String),
+    //
+    InvalidSize(String),
+    //
+    InvalidColorType(String),
+    //
+    InvalidImageDepthSize(String),
+    //
+    InsufficientBufferSize(String),
 }
 
-/// Build error value if any error occurs during runtime
-///
-/// # Examples
-/// ```
-/// use vision_x::{errors::Error as ImageError, image};
-///
-/// fn get_image() -> Result<(), ImageError> {
-///     let path = "images/png/xyz.png";
-///     let image_data = image::load(&path)?;
-///
-///     Ok(())
-/// }
-/// ```
-#[derive(Debug, Clone)]
-pub struct Error {
-    pub message: String,
-    pub code: ErrorCode,
-}
-
-impl Error {
-    /// Returns the error object
-    pub fn custom_error<T>(self) -> Result<T, Self> {
-        Err(self)
+/// Implements `fmt::Display` for our custom Errorkind
+impl fmt::Display for VisionXErrorKind {
+    ///
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            VisionXErrorKind::IndexOutofBound(err) => write!(f, "index error at {err}"),
+            VisionXErrorKind::InvalidSize(err) => write!(f, "size error while {err}"),
+            VisionXErrorKind::InvalidColorType(err) => {
+                write!(f, "invalid color codec used while {err}")
+            }
+            VisionXErrorKind::InvalidImageDepthSize(err) => {
+                write!(f, "invalid color depth size is used to {}", err)
+            }
+            VisionXErrorKind::InsufficientBufferSize(err) => {
+                write!(f, "buffer size error while {err}")
+            }
+        }
     }
 }
+
+/// Implements `std::error::Error` for our custom Errorkind
+impl std::error::Error for VisionXErrorKind {}
